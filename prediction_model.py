24c16ffcd7dd88339ec0ec099e2a1d1258a88ab6
mpollak <marcellopollak123@gmail.com>
2025-07-08 14:53:42 -0400

    added code to generate a simple empire or resistance plot

--- prediction_model.py
+++ prediction_model.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 troop_movements = pd.read_csv('troop_movements.csv')
 empire_or_resistance_counts = troop_movements['empire_or_resistance'].value_counts()
@@ -6,4 +8,6 @@ homeworld_counts = troop_movements['homeworld'].value_counts()
 unit_type_counts = troop_movements['unit_type'].value_counts()
 troop_movements['is_resistance'] = troop_movements['empire_or_resistance'] == 'resistance'
 
-
+sns.countplot(x='empire_or_resistance', data=troop_movements)
+plt.title('Character Count by Empire or Resistance') 
+plt.show() 
