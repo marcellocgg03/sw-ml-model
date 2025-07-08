2e68096075acc0f8a7690cce6678741001a50ec2
mpollak <marcellopollak123@gmail.com>
2025-07-08 14:47:21 -0400

    added reading of troop_movements.csv into a pd dataframe

--- prediction_model.py
+++ prediction_model.py
@@ -1,2 +1,9 @@
 import pandas as pd
 
+troop_movements = pd.read_csv('troop_movements.csv')
+empire_or_resistance_counts = troop_movements['empire_or_resistance'].value_counts()
+homeworld_counts = troop_movements['homeworld'].value_counts()
+unit_type_counts = troop_movements['unit_type'].value_counts()
+troop_movements['is_resistance'] = troop_movements['empire_or_resistance'] == 'resistance'
+
+
