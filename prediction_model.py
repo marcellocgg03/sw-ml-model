a50cc3064f68bc98ba4aa74e7eb972202aea0411
mpollak <marcellopollak123@gmail.com>
2025-07-08 15:28:33 -0400

    Added a DecisionTreeClassifier

--- prediction_model.py
+++ prediction_model.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
 
 troop_movements = pd.read_csv('troop_movements.csv')
 empire_or_resistance_counts = troop_movements['empire_or_resistance'].value_counts()
@@ -10,4 +13,21 @@ troop_movements['is_resistance'] = troop_movements['empire_or_resistance'] == 'r
 
 sns.countplot(x='empire_or_resistance', data=troop_movements)
 plt.title('Character Count by Empire or Resistance') 
-plt.show() 
+
+x=troop_movements[['homeworld', 'unit_type']]
+x_encoded=pd.get_dummies(x)
+y=troop_movements['is_resistance']
+
+X_train, X_test, Y_train, Y_test = train_test_split(x_encoded, y, test_size=0.3, random_state=42)
+model = DecisionTreeClassifier()
+model.fit(X_train, Y_train)
+accuracy = model.score(X_test, Y_test)
+
+importances = model.feature_importances_
+feature_importances = pd.DataFrame({'Feature': x_encoded.columns, 'Importance': importances})
+feature_importances = feature_importances.sort_values(by='Importance', ascending=False)
+plt.figure(figsize=(10,12))
+sns.barplot(x='Feature', y='Importance', data=feature_importances)
+plt.xticks(rotation=90)
+plt.title('Feature Importances')
+plt.show()
