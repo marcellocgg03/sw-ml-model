ed0d4d0fcfc84ab7c586c5e4fae94f720f8b717d
mpollak <marcellopollak123@gmail.com>
2025-07-09 09:52:49 -0400

    added DecisionTree printing to testing_model.py

--- testing_model.py
+++ testing_model.py
@@ -1,5 +1,10 @@
 import pickle as pkl
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import plot_tree
+from sklearn import tree
+from sklearn.tree import export_text
 
 with open('trained_model.pkl', 'rb') as f:
     model = pkl.load(f)
@@ -8,4 +13,5 @@ data=pd.read_parquet('troop_movements_1m.parquet')
 x=data[['homeworld', 'unit_type']]
 x_encoded=pd.get_dummies(x)
 data['predictions'] = model.predict(x_encoded)
-print(data.head())
+text_tree = tree.export_text(model, feature_names=x_encoded.columns)
+print(text_tree)
