c16c29821f9f6b89bd62bd840cacff1b37ddb287
mpollak <marcellopollak123@gmail.com>
2025-07-09 09:52:21 -0400

    fixed "unkown" typo in generate_parquet.py

--- generate_parquet.py
+++ generate_parquet.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 troop_movements = pd.read_csv('troop_movements_1m.csv')
-troop_movements['unit_type']=troop_movements['unit_type'].replace('invalid_unit', 'unkown')
+troop_movements['unit_type']=troop_movements['unit_type'].replace('invalid_unit', 'unknown')
 troop_movements[['location_x', 'location_y']]=troop_movements[['location_x', 'location_y']].fillna(method='ffill')
 troop_movements.to_parquet('troop_movements_1m.parquet', engine='pyarrow')
